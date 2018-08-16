5928ec1a1ea0c875f456e9f2de364c6e7a156112
Maksym Pugach <pugach.m@gmail.com>
2018-08-16 09:06:36 +0300

    add unprivileged version

---
+++ 0.13.2-unpriv/docker-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+if [[ $(echo "$1" | cut -c1) = "-" ]]; then
+  echo "$0: assuming arguments for vertcoind"
+
+  set -- vertcoind "$@"
+fi
+
+if [[ $(echo "$1" | cut -c1) = "-" ]] || [[ "$1" = "vertcoind" ]]; then
+  mkdir -p "$VERTCOIN_DATA"
+  chmod 700 "$VERTCOIN_DATA"
+  chown -R vertcoin "$VERTCOIN_DATA"
+
+  echo "$0: setting data directory to $VERTCOIN_DATA"
+
+  set -- "$@" -datadir="$VERTCOIN_DATA"
+fi
+
+if [[ "$1" = "vertcoind" ]] || [[ "$1" = "vertcoin-cli" ]] || [[ "$1" = "vertcoin-tx" ]]; then
+  echo
+  exec gosu vertcoin "$@"
+fi
+
+echo
+exec "$@"
